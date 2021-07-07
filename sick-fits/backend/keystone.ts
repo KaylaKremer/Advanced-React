5004d00e80511a073e3afd8b8f95be01f12f5001
Kayla Kremer <kaylakremer@icloud.com>
2021-07-07 00:52:35 -0400

    add authentication

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,5 +1,10 @@
 import 'dotenv/config';
 import { createSchema, config } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
 import { User } from './schemas/Users';
 
 const databaseURL =
@@ -7,24 +12,44 @@ const databaseURL =
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360,
-  secret: 'SICK FITSSSSS',
+  secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
-  },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-  },
-  lists: createSchema({
-    User,
-  }),
-  ui: {
-    isAccessAllowed: () => true,
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
   },
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+    },
+    lists: createSchema({
+      // Schema items go here
+      User,
+    }),
+    ui: {
+      // Show the UI for people who pass this test
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL query
+      User: 'id',
+    }),
+  })
+);
