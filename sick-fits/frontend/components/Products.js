8d68be35f0d60d4263410a0d71f13099d3a6a772
Kayla Kremer <kaylakremer@icloud.com>
2021-07-08 19:53:58 -0400

    add extra control over Apollo cache for pagination

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -6,7 +6,7 @@ import { perPage } from '../config';
 
 export const ALL_PRODUCTS_QUERY = gql`
   query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
-    allProducts(first: $first, skip: $skip) {
+    allProducts(skip: $skip, first: $first) {
       id
       name
       price
