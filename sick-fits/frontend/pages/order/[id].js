789abfa3c3a2e1f310e417097563793b8b5faaf5
Kayla Kremer <kaylakremer@icloud.com>
2021-07-11 03:34:27 -0400

    create page for displaying all orders

--- sick-fits/frontend/pages/order/[id].js
+++ sick-fits/frontend/pages/order/[id].js
@@ -6,7 +6,7 @@ import OrderStyles from '../../components/styles/OrderStyles';
 import formatMoney from '../../lib/formatMoney';
 
 const SINGLE_ORDER_QUERY = gql`
-  query SINGLE_ITEM_QUERY($id: ID!) {
+  query SINGLE_ORDER_QUERY($id: ID!) {
     order: Order(where: { id: $id }) {
       id
       charge
